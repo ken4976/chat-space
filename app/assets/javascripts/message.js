d37126a8ec7f9fcaa372b471c3af24c27bbc0d2d
森田賢 <moritaken@moritakennoMacBook-puro.local>
2018-09-13 21:34:10 +0900

    fifth commit

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,18 +1,22 @@
 $(function(){
 function buildHTML(message){
-  var html = `<div class = content-main__message>
-<div class = content-main__message__name></div>
-  ${message.user_name}
-<div class = content-main__message__days></div>
-  ${message.created_at}
-<div class = content-main__message__message></div>
-  ${message.content}
+  var html =`<div class = content-main__message>
+<div class = content-main__message__name>
+${message.user_name}
+</div>
+<div class = content-main__message__days>
+${message.created_at}
+</div>
+<div class = content-main__message__message>
+${message.content}
+</div>
 </div>`
   return html;
 }
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
+    $('#button').removeAttr('data-disable-with')
     var url = $(this).attr('action')
     $.ajax({
       url: url,
@@ -24,11 +28,11 @@ function buildHTML(message){
   })
     .done(function(data){
       var html = buildHTML(data);
-      $('.content-main__message').append(html)
+      console.log(html);
+      $('.content-main').append(html)
       $('.form__message').val('')
-      .fail(function(){
-      alert('error');
-});
+      $('.content-main').animate({scrollTop: $('.content-main')[0].scrollHeight}, 'fast');
+
 });
 });
 });
